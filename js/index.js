72a89b06b5613a4d60993c45d3f9c58d385cba52
Oleh <trubatur@ukr.net>
2019-04-03 11:48:39 +0000

    can't solve 3 and 5

--- js/index.js
+++ js/index.js
@@ -1,29 +1,8 @@
 /* 1st Kata 
 
-function getLength(arr){
-  //return length of arr
-  return arr.length
-}
-function getFirst(arr){
-  //return the first element of arr
-  var items = list.slice(0, size)
-  return items
-}
-function getLast(arr){
-  //return the last element of arr
-  var items = list.slice(0, size)
-  return items
-}
-function pushElement(arr){
-  var el=1;
-  //push el to arr
-  arr.push(el)
-  return arr
-}
-function popElement(arr){
-  //pop an element from arr
-  arr.pop()
-  return arr
+function circleCircumference(circle) {
+const p=3.14159265358979;
+ return 2*p*circle.radius;
 }
 
 */
@@ -31,17 +10,23 @@ function popElement(arr){
 /* 2nd Kata
 
 
-function grabDoll(dolls){
-  var bag=[];
-  for(var i = 0; i < dolls.length; i++) {
-    if(dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll")
-      bag.push(dolls[i]);
-    else
-      continue;
-      
-    if(bag.length === 3) break;
-  }
-  return bag;
+function whatNumberIsIt(n){
+  if (isNaN(n))
+    return "Input number is Number.NaN";
+
+  switch(n) {
+    case Number.MAX_VALUE:
+      return "Input number is Number.MAX_VALUE";
+    case Number.MIN_VALUE:
+      return "Input number is Number.MIN_VALUE";
+    case -Infinity:
+      return "Input number is Number.NEGATIVE_INFINITY";
+    case Infinity:
+      return "Input number is Number.POSITIVE_INFINITY";
+    default:
+      return "Input number is " + n;
+  }  
+}
 }
 
 */
@@ -50,9 +35,6 @@ function grabDoll(dolls){
 
 /* 3rd Kata
 
-function DNAtoRNA(dna) {
-  return dna.split("T").join("U");
-}
 
 */
 
@@ -61,22 +43,21 @@ function DNAtoRNA(dna) {
 
 /* 4th Kata
 
-function splitAndMerge(str,sp){
-  var words = str.split(" ");
-  var arr =[];
-  var res = []
-  for(i=0;i<words.length;i++){
-   arr.push(words[i].split("").join(sp));
-  }
-  res = arr.join(" ");
-  return res;
-  
+function buildFun(n){
+
+  var result = []
+
+for (let i = 0; i < n; i++) {
+  result[i] = function () {
+   return i
+  };
+}
+   return result
 }
 */
 
 /* 5th Kata
 
-Не можу розібратись
 
 */
 
