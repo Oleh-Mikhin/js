bb1f848bdd569dd3e151bfaed722d476a75e6408
Oleh <trubatur@ukr.net>
2019-04-03 12:46:58 +0000

    2nd Kata  repair

--- js/index.js
+++ js/index.js
@@ -11,23 +11,19 @@ const p=3.14159265358979;
 
 
 function whatNumberIsIt(n){
-  if (isNaN(n))
-    return "Input number is Number.NaN";
-
-  switch(n) {
+  switch (n) {
+    case Number.POSITIVE_INFINITY:
+      return 'Input number is Number.POSITIVE_INFINITY';
+    case Number.NEGATIVE_INFINITY:
+      return 'Input number is Number.NEGATIVE_INFINITY';
     case Number.MAX_VALUE:
-      return "Input number is Number.MAX_VALUE";
+      return 'Input number is Number.MAX_VALUE';
     case Number.MIN_VALUE:
-      return "Input number is Number.MIN_VALUE";
-    case -Infinity:
-      return "Input number is Number.NEGATIVE_INFINITY";
-    case Infinity:
-      return "Input number is Number.POSITIVE_INFINITY";
-    default:
-      return "Input number is " + n;
-  }  
+      return 'Input number is Number.MIN_VALUE';
+  }
+  return isNaN(n) ? 'Input number is Number.NaN' : `Input number is ${n}`
 }
-}
+
 
 */
 
